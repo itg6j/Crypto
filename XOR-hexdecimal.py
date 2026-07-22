a4fd3721bcc6b9bccbf1a1d1953b0fc0e767d913
Saif <145370684+itg6j@users.noreply.github.com>
2026-07-22 11:57:55 +0300

    Add files via upload

---
+++ XOR-hexdecimal.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import sys
+if sys.version_info.major == 2:
+    print("You are running Python 2, which is no longer supported. Please update to Python 3.")
+ords = [81, 64, 75, 66, 70, 93, 73, 72, 1, 92, 109, 2, 84, 109, 66, 75, 70, 90, 2, 92, 79]
+for i in ords : 
+    x = chr(i^0x32)
+    print("".join(x),end="")
