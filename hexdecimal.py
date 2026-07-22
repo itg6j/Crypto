5f691751c4f11ce9cc83df08b34c81de6d665fae
Saif <145370684+itg6j@users.noreply.github.com>
2026-07-22 12:10:57 +0300

    Add files via upload

---
+++ hexdecimal.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+hex = "63727970746f7b596f755f77696c6c5f62655f776f726b696e675f776974685f6865785f737472696e67735f615f6c6f747d"
+de = bytes.fromhex(hex).decode('utf-8')
+print("flag : "+de)
+
+#!/usr/bin/env python3
+hex = "crypto{You_will_be_working_with_hex_strings_a_lot}"
+x = hex.encode()
+v = x.hex()
+print(v)
