83cc000c70ae6859c2e943cc4522a2f71d17d4eb
Saif <145370684+itg6j@users.noreply.github.com>
2026-08-24 17:08:24 +0300

    Update ExtendedEuclideanAlgorithm.py

--- ExtendedEuclideanAlgorithm.py
+++ ExtendedEuclideanAlgorithm.py
@@ -1,3 +1,4 @@
+from math import gcd
 def extendEuclidean(a, b): 
     s1, s2 = 1, 0
     t1, t2 = 0, 1    
@@ -10,17 +11,16 @@ def extendEuclidean(a, b):
         t = t1 - q * t2
         s1, s2 = s2, s
         t1, t2 = t2, t
-    return a, s1, t1
+    return s1, t1
 num1 = int(input("[+] Enter a : "))
 num2 = int(input("[+] Enter b : "))
-gcd, x, y = extendEuclidean(num1, num2) 
-print("[+] GCD =", gcd)
+x, y = extendEuclidean(num1, num2) 
+gcd1 = gcd(num1,num2)
+print("[+] GCD =", gcd1)
 print("[+] x =", x)
 print("[+] y =", y)
 z = (x*num1)+(y*num2)
 status = None
-if gcd == z : 
+if gcd1 == z : 
     status = True
-else : 
-    status = False
 print(f"[+] if s * a + t * b = gcd(a,b) ??? result = {z}, gcd = result : {status}")
