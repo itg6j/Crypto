8bcb94643285777ee3fd4cd49b283828110466c1
Saif <145370684+itg6j@users.noreply.github.com>
2026-08-24 18:53:41 +0300

    Update Greates-Common-Divisor.py

--- Greates-Common-Divisor.py
+++ Greates-Common-Divisor.py
@@ -1,3 +1,9 @@
-from Crypto.Util.number import * 
-x = GCD (84,60,150)
-print(x)
+from math import gcd
+from functools import reduce 
+num = int(input("[+] Enter num of range : "))
+number = []
+for i in range(0,num) : 
+    x = int(input(f"[+] Enter number {i+1} : "))
+    number.append(x)
+y = reduce(gcd,number)
+print("[+] The Grates Common Divisor : ",y)
