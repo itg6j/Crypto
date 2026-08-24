b025d6f6e6d2a6c704dc8bf025e3e9f9d8e26887
Saif <145370684+itg6j@users.noreply.github.com>
2026-08-24 17:35:35 +0300

    Update Pollard's P-1 algorithm for user input
    Modified Pollard's P-1 algorithm to accept user input for modulus and improved base selection logic.

--- Pollard'sP-1.py
+++ Pollard'sP-1.py
@@ -1,21 +1,25 @@
 from math import gcd, factorial
-n = 2993
+import sys
+n = int(input("[+] Enter modulus : "))
 a = 2
+if gcd(n,a) != 1 : 
+    for i in range(3,n) : 
+        if gcd(n,i) == 1 : 
+            a = i
+            break
 if gcd(n,a) == 1 : 
     k1 = 1
     while True : 
-        k = factorial(k1)
+        k = factorial(k1)#1!
         e = pow(a,k,n)
         h= e % n
         p = gcd(h-1,n)
         q = n//p
         if p == 1 : 
             k1 = k1+1 
+        elif p == n:
+            print("[+] Failed with this base a, try another one")
+            sys.exit()
         else : 
             break
     print(f"[+] factor p is :  {p} q : {q}")
-else : 
-    for i in range(3,n) : 
-        if gcd(n,i) == 1 : 
-            a = i
-            break
