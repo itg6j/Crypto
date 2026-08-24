02d65df7f8feb462664d15d0b2fe4108a672caf8
Saif <145370684+itg6j@users.noreply.github.com>
2026-08-24 16:31:01 +0300

    Update pollardRho.py

--- pollardRho.py
+++ pollardRho.py
@@ -1,17 +1,18 @@
-from Crypto.Util.number import GCD 
-n = 94417
-a = 2
-b = 2
+from math import gcd
+n = int(input("[+] Enter n : "))
+a = int(input("[+] Enter a : "))
+b = int(input("[+] Enter b : "))
 d = 0
 for i in range(n) : 
     a = ((a**2)+1) %n
     b = ((b**2)+1) %n
     b = ((b**2)+1) %n
-    d = GCD((a-b),n)
+    d = gcd((a-b),n)
     if 1<d and d<n and d != 1 : 
         print("p = : ",d)
-        print("q = : ",n//d)
-        n1 = d * (n//d)
+        q = n//d
+        print("q = : ",q)
+        n1 = d * q
         if n == n1 : 
-            print("input equal output ? : ",True)
+            print(f"[+] {n1} = {d} x {q}: ",True)
         break 
