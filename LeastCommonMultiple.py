9978cb90cf47db751089a04f3213131a516a9926
Saif <145370684+itg6j@users.noreply.github.com>
2026-08-24 19:01:57 +0300

    Update LeastCommonMultiple.py

--- LeastCommonMultiple.py
+++ LeastCommonMultiple.py
@@ -1,10 +1,12 @@
-from Crypto.Util.number import * 
-
-def GCD1(a,b) : 
-    x = GCD(a,b)
-    return x
-a = 12
-b = 18
-y = a*b
-z = y/GCD1(a,b)
-print(z)
+from math import gcd
+from functools import reduce 
+number = []
+num = int(input("[+] Enter range : "))
+multiple = 1
+for i in range(0,num) : 
+    x = int(input(f"[+] Enter number {i+1} : "))
+    number.append(x)
+def lcm(a, b):
+    return a * b // gcd(a, b)
+y = reduce(lcm,number)
+print(f"[+] LCM = {y}")
