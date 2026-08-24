a0072f124a1c04d9734a5f488c97b20016f641ef
Saif <145370684+itg6j@users.noreply.github.com>
2026-08-24 22:27:14 +0300

    Update RSAh.py

--- RSAh.py
+++ RSAh.py
@@ -1,6 +1,8 @@
 from math import isqrt,gcd
 import time 
 import random 
+import sys
+import secrets
 def decryption(cipher, d, n):
     decrypted_blocks, h, s = sam(cipher, d, n)
     message = b""
@@ -12,16 +14,18 @@ def decryption(cipher, d, n):
     return message, h, s
 def decode(cipher, d, n):
     c = str(cipher)
+    BlockSize = len(str(n))
     cipher_list = []
-    for i in range(0, len(c), 13):
-        block_str = c[i:i+13]
+    for i in range(0, len(c), BlockSize):
+        block_str = c[i:i+BlockSize]
         cipher_list.append(int(block_str))
     message,h,s = decryption(cipher_list,d,n)
     return message.decode('ascii', errors='ignore').rstrip(),h,s
 def encryption(en,e,n):
     m = encode(en) 
     blocks=[]
-    BlockSize = 5
+    max_bytes = (n.bit_length() - 1) // 8
+    BlockSize = max(1, max_bytes)
     for i in range(0,len(m),BlockSize):
         block = m[i:i+BlockSize]
         count = 0
@@ -29,18 +33,16 @@ def encryption(en,e,n):
             count = BlockSize - len(block)
             block = block + (' ' * count).encode('utf-8')
         num = int.from_bytes(block,'big')
+        if num>= n : 
+            print("[+] message greater then modulus")
+            sys.exit()
         blocks.append((num))
     cipher , count1 , status = sam(blocks,e,n)
     return cipher,count1,status
-# encode ascii int , count padding 
+# encode ascii int , count padding  
 def encode(message) : 
     m = message.encode('ascii')
-    filteM = bytes([b for b in m if 32 <= b <= 127])
-    return filteM# encode ascii int , count padding 
-def encode(message) : 
-    m = message.encode('ascii')
-    filteM = bytes([b for b in m if 32 <= b <= 127])
-    return filteM
+    return m
 # sam1: c or m count and padding  |||| count1 : num of square , ,multiplication and if == pow
 def sam(base,exp,mod): 
     e = bin(exp)[2:]
@@ -101,7 +103,7 @@ def Euler1(number) :
         return number-1
 def Euler2 (number1,number2) : 
     z = GCD1(number1,number2)
-    if number1 == number2 and z == True : 
+    if number1 == number2 and z == False : 
         return (number1-1)*number2
     if number1!=number2 and z == True: 
         z = Euler1(number1)
@@ -133,6 +135,8 @@ def RSAKEY(p,q) :
     else : 
         return"[-] factor is not prime"
 def trialDivision(n):
+    start_time = time.time()  
+    timeout = 5 
     x = isqrt(n)
     p=x
     q=x
@@ -146,14 +150,10 @@ def trialDivision(n):
         q=q-1
     return p,q
 def PollardRho(n): 
-    start_time = time.time()  
-    timeout = 5 
     a = 2
     b = 2
     d = 0
     while True:
-        if time.time() - start_time > timeout:
-            return None
         a = ((a**2)+1) %n
         b = ((b**2)+1) %n
         b = ((b**2)+1) %n
@@ -162,24 +162,32 @@ def PollardRho(n):
             p = d
             q = n//d
             return p,q
-
+def generateRandomPrime(bits) : 
+    while True : 
+        x = (secrets.randbits(bits) | (1<<(bits-1)))|1
+        if isPrime(x) : 
+            return x
 choose1 = input("[+] Do you want encryption or decryption ? e/d : ")
 if choose1 == 'e' : 
-    choose3 = input("[+] Do you want to encrypt it with your public key or with another key? 0/1 : ")
+    choose3 = input("[+] Do you want to encrypt it with your public key or with another key? 0/1: ")
     if choose3 == "0" : 
         e = int(input("[+] Enter encryption key : "))
         n = int(input("[+] Enter n : "))
         mc = input("[+] Enter message : ")
-    else : 
-        choose = input("[+] Do you want change factor ? y/n : ")
+    else: 
+        choose = input("[+] Do you want change factor  or generate random prime number you determine  of factor? y/n/g : ")
         if choose == 'y' : 
             p1 = int(input("[+] write first factor : "))
             q1 = int(input("[+] write second factor : "))
             p = p1
             q = q1
-        else : 
+        elif choose == 'n' : 
             p = 2097143
             q = 2097169
+        else : 
+            size= int(input("[+] what size do you want such as 10 mean 10bits for one factor: "))
+            p = generateRandomPrime(size)
+            q = generateRandomPrime(size)
         mc = input("[+] Enter message : ")
         key = RSAKEY(p,q)
         n = key[0]
@@ -188,6 +196,7 @@ if choose1 == 'e' :
         d = key[3]
         cipher,count,status = encryption(mc,e,n)
         x = "".join([str(c) for c in cipher])
+        print(f"[+] prime number is {p} , {q}")
         print(f"[+] the encryption key is : {e}")
         print(f"[+] the dencryption key is : {d}")
         print(f"[+] the modula n {n}")
