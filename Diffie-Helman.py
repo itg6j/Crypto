d86d7579f201709b67a5c0696d0c6409d67b160c
Saif <145370684+itg6j@users.noreply.github.com>
2026-08-24 17:25:04 +0300

    Update Diffie-Helman.py

--- Diffie-Helman.py
+++ Diffie-Helman.py
@@ -1,10 +1,17 @@
-q = 17
-alpha = 2
-Xa = 3
-Xb = 5
+from sympy import isprime
+import sys
+q = int(input("[+] Enter Prime modulus : "))
+x = isprime(q)
+if x == False : 
+    sys.exit()
+alpha = int(input("[+] Enter primitive root or alpha : "))
+Xa = int(input("[+] Enter private key for first person : "))
+Xb = int(input("[+] Enter private key for second person : "))
 b = pow(alpha,Xa,q)
 a = pow(alpha,Xb,q)
 k1 = pow(b,Xb,q)
 k2 = pow(a,Xa,q)
+status = None
 if k1 == k2 : 
-    print(f"[+] key is : {k1}")
+    status = True
+    print(f"[+] if key first person equal key second person :  {status}  , key is : {k1}")
