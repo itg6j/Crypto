56c6c64ca51a679e318a7bfa7b668e73f2b8e5ee
Saif <145370684+itg6j@users.noreply.github.com>
2026-08-24 17:27:37 +0300

    Update DLP.py

--- DLP.py
+++ DLP.py
@@ -1,5 +1,11 @@
-q = 991
-g = 209
+from sympy import isprime
+import sys
+q = int(input("[+] Enter prime modulus : "))
+x = isprime(q)
+if x == False : 
+    print("[+] not prime modulus")
+    sys.exit()
+g = int(input("[+] Enter generator : "))
 for i in range(q) : 
     x = g*i%q
     if x == 1 : 
