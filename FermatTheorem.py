2ae6e407234a775d6057d6c3e4f976bfb5e4ceeb
Saif <145370684+itg6j@users.noreply.github.com>
2026-08-24 17:45:02 +0300

    Update FermatTheorem.py

--- FermatTheorem.py
+++ FermatTheorem.py
@@ -1,6 +1,19 @@
+from math import gcd
+from sympy import isprime
+import sys
 def fermat(number,exponent,prime) : 
     x = exponent % (prime-1)
     result  = pow(number,x,prime)
     return result
-y = fermat(4,532,11)
-print("result :",y)
+a = int(input("[+] Enter number : "))
+e = int(input("[+] Enter exponention : "))
+p = int(input("[+] Enter prime modulus : "))
+x = isprime(p)
+if x== False: 
+    print("[+] modulus not prime")
+    sys.exit
+if gcd(a,p) == 1 : 
+    y = fermat(4,532,11)
+    print(f"[+] result : {y} mod {p}")
+else : 
+    print("[+] gcd != 1")
