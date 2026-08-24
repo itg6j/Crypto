2e49335598eed9fa207694ffe86c06612e8d689b
Saif <145370684+itg6j@users.noreply.github.com>
2026-08-24 16:40:52 +0300

    Update Square-and-MultiplyAlgorithm.py

--- Square-and-MultiplyAlgorithm.py
+++ Square-and-MultiplyAlgorithm.py
@@ -1,10 +1,16 @@
-b = 4
-e = 13
-n = 49
+b = int(input("[+] Enter base : "))
+e = int(input("[+] Enter encryption key : "))
+n = int(input("[+] Enter n : "))
 bits = bin(e)[2:]
+square = 0
+multiplication = 0
 x = 1
 for i in bits : 
     x = (x * x) % n
+    square = square +1
     if i == '1':
         x = (x * b) % n
-print(x)
+        multiplication = multiplication +1
+print(f"[+] The result is : {x}")
+print(f"[+] Round of square : {square}")
+print(f"[+] Round of multiplication : {multiplication}")
