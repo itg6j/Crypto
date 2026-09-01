4154d1acf1283262af27e7be73919a1f49e58351
Saif <145370684+itg6j@users.noreply.github.com>
2026-09-01 17:31:31 +0300

    Refactor Euler2 function and update input prompts

--- EulerTotientFunction.py
+++ EulerTotientFunction.py
@@ -9,13 +9,13 @@ def Euler1(number):
     return count
 def GCD1(number1, number2):
     return gcd(number1, number2) == 1
-def Euler2(number1, number2):
-    if GCD1(number1, number2):
+def Euler2 (number1,number2) : 
+    z = GCD1(number1,number2)
+    if z == True: 
         return Euler1(number1) * Euler1(number2)
     else:
         return Euler1(number1 * number2)
-
-num1 = int(input("Enter number 1 : "))
-num2 = int(input("Enter number 2 : "))
+num1 = int(input("[+] Enter number 1 : "))
+num2 = int(input("[+] Enter number 2 : "))
 x = Euler2(num1,num2)
-print(f"The result  : {x}") 
+print(f"[+] result : {x}")
