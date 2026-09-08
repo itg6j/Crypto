90b11d00188d0ed9e4b1a5c75ab7e277ed4bcf60
Saif <145370684+itg6j@users.noreply.github.com>
2026-09-08 04:21:43 -0400

    Implement AES encryption and decryption in AESECB.py

---
+++ AESECB.py
@@ -0,0 +1,27 @@
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad, unpad
+key = input("[+] Enter key size(16,24,32)byte: ")
+flag = input("[+] Enter message or ciphertext : ")
+KEY = key.encode()
+FLAG = flag
+choose = input("[+] Do you want encrypt or decrypt (e/d): ")
+if choose == "d" : 
+    def decrypt(ciphertext):
+        ciphertext = bytes.fromhex(ciphertext)
+        cipher = AES.new(KEY, AES.MODE_ECB)
+        try:
+            decrypted_padded = cipher.decrypt(ciphertext)
+            decrypted = unpad(decrypted_padded, AES.block_size)
+            return("plaintext :", decrypted.decode())
+        except ValueError as e:
+            return {"error": str(e)}
+    c_hex = decrypt(FLAG)
+    print(c_hex)
+else : 
+    def encrypt_flag():
+        cipher = AES.new(KEY, AES.MODE_ECB)
+        padded_data = pad(FLAG.encode(), AES.block_size)
+        encrypted = cipher.encrypt(padded_data)
+        return("ciphertext :", encrypted.hex())
+    c_hex = encrypt_flag()
+    print(c_hex)
