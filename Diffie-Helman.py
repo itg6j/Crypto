e51ab689d98018544161e63e2c484a442c1e2484
Saif <145370684+itg6j@users.noreply.github.com>
2026-09-12 12:30:05 -0400

    Update Diffie-Helman.py

--- Diffie-Helman.py
+++ Diffie-Helman.py
@@ -1,14 +1,15 @@
 import sys
 from sympy import isprime
+import time
 q = int(input("[+] Enter Prime modulus (p/q): "))
 if not isprime(q):
     print("[-] Error: Modulus must be a prime number!")
     sys.exit()
 alpha = int(input("[+] Enter primitive root (g/alpha): "))
 print("\n--- Current Knowledge Setup ---")
-has_Xa = input("[+] Do you know Alice's private key (Xa)? (y/n): ")
+has_Xa = input("[+] Do you know Alice's private key (Xa)? (y/n): ") == "y"
 Xa = int(input("[+] Enter Alice's private key: ")) if has_Xa else None
-has_Xb = input("[+] Do you know Bob's private key (Xb)? (y/n): ")
+has_Xb = input("[+] Do you know Bob's private key (Xb)? (y/n): ") == "y"
 Xb = int(input("[+] Enter Bob's private key: ")) if has_Xb else None
 if Xa is not None:
     A = pow(alpha, Xa, q)
@@ -32,4 +33,18 @@ elif k1 is not None:
 elif k2 is not None:
     print(f"[+] Computed Shared Secret from Bob's side: {k2}")
 else:
-    print("[-] Infeasible: Need at least one private key to compute the shared secret.")
+    print("\n[+] Infeasible: Need at least one private key to compute the shared secret.\n")
+    print("="*45)
+    print("\n[+]Brute force")
+    count = 1
+    strtime = time.time()
+    while True : 
+        x = pow(alpha,count,q)
+        if time.time() - strtime>=60:
+            break
+        count = count + 1
+        if x == A : 
+            print(f"[+] private key A = {x}")
+            exit()
+    print("\n[+]Bruteforce Faild")
+    
