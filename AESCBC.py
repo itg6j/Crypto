45d0316339ada17c49a0f4c7f3c34420a32f8fb7
Saif <145370684+itg6j@users.noreply.github.com>
2026-09-12 14:38:16 -0400

    Create AESCBC.py

---
+++ AESCBC.py
@@ -0,0 +1,22 @@
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad,unpad
+import os
+key = input("[+] Enter key (must be 16, 24, or 32 characters): ").encode()
+flag = input("[+] Enter message : ").encode()
+if len(key) not in (16,24,32) : 
+    print("[+] Error: Key length must be exactly 16, 24, or 32 bytes.")
+    exit()
+choose = input("[+] Do you want encrypt or decrypt (e/d): ")
+if choose == "e" : 
+    iv = os.urandom(16)
+    cipher = AES.new(key,AES.MODE_CBC,iv)
+    ciphertext = cipher.encrypt(pad(flag,AES.block_size))
+    civ = iv+ciphertext
+    print(civ.hex())
+else :
+    raw = bytes.fromhex(flag.decode())
+    riv = raw[:16]
+    ciphertext = raw[16:]
+    decipher = AES.new(key,AES.MODE_CBC,riv)
+    deciphertext = unpad(decipher.decrypt(ciphertext),AES.block_size)
+    print(deciphertext.decode())
