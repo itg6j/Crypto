42f965b0154f2d4e544a00fee35d03ef6ddc0614
Saif <145370684+itg6j@users.noreply.github.com>
2026-09-17 12:08:39 -0400

    Update print statement for collision attempts output

--- hash/Collision.py
+++ hash/Collision.py
@@ -4,4 +4,4 @@ num1 = float(input("[+] Enter target probability percentage (e.g., 50) : "))
 P = num1/100
 space = 2**num
 k = math.sqrt(2 * space * math.log(1 / (1 - P)))
-print(f"[+] {math.ceil(k)}")
+print(f"[+] number of attempts (hashes) required to find a collision {math.ceil(k)}")
