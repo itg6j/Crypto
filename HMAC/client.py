f01fbb54d3661aa197670a6393fea963b285239a
Saif <145370684+itg6j@users.noreply.github.com>
2026-09-17 15:25:46 -0400

    Create client.py

---
+++ HMAC/client.py
@@ -0,0 +1,32 @@
+import socket 
+import hashlib
+import hmac
+import threading
+def receive_messages(sock):
+    key = b"hello world"
+    while True:
+        try:
+            data = sock.recv(4096).decode()
+            if not data:
+                print("\n[-] Client disconnected.")
+                break
+            list1 = data.split(",")
+            message = list1[0]
+            hmac1 = list1[1]
+            hmac2 = hmac.new(key,bytes(message,'utf-8'),hashlib.sha256).hexdigest()
+            if hmac1 == hmac2 :
+                print(f"\n[Client]: {message}\n[You]: ", end="")
+            else : 
+                print("[+] The message altered")
+        except:
+            break
+c = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+c.connect(("127.0.0.1",5000))
+threading.Thread(target=receive_messages, args=(c,), daemon=True).start()
+key = b"hello world"
+while True :
+    message = input("[+] Enter message : ")
+    hmac1 = hmac.new(key,bytes(message,'utf-8'),hashlib.sha256).hexdigest()
+    msg = message+","+hmac1
+    c.sendall(msg.encode())
+c.close()
