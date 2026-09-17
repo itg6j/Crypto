bb9ad932cc40ea4153988379674a4bc061c3c08d
Saif <145370684+itg6j@users.noreply.github.com>
2026-09-17 12:13:42 -0400

    Update SecondPreimageResistance.py

--- hash/SecondPreimageResistance.py
+++ hash/SecondPreimageResistance.py
@@ -4,5 +4,8 @@ num1 = float(input("[+] Enter target probability percentage (e.g., 50) : "))
 percent = num1/100
 space = 2**num
 p = 1/space
-k = math.log(1-percent)/math.log(1-p)
-print(math.ceil(k))
+try:
+    k = math.log(1-percent)/math.log(1-p)
+except Exception : 
+    k = math.log(1 / (1 - percent)) * space
+print(f"[+] number of attempts required to match a specific {math.ceil(k)}")
